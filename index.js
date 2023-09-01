80d8e58f60d16e3628dc9a82db1987c4315d765a
Cheekay <chikeonyema@gmail.com>
2023-09-01 14:23:41 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import { menuArray } from "/data.js";
+
+function returnMenuArray(menuArr) {
+	return menuArr
+		.map((item) => {
+			const { name, ingredients, id, price, emoji } = item;
+			return `
+            <div class="item">
+					<div class="emoji">${emoji}</div>
+					<div class="item-details">
+						<h3 class="item-name">${name}</h3>
+						<p class="item-desc">${ingredients}</p>
+						<h4 class="item-price">${price}</h4>
+					</div>
+						<button id="add-btn">+</button>
+				</div>
+        `;
+		})
+		.join("");
+}
+
+document.getElementById("itemList").innerHTML = returnMenuArray(menuArray);
