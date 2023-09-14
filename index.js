4aefe612cd5f1443bbb52249e58fe23f509269ee
Cheekay <chikeonyema@gmail.com>
2023-09-13 22:00:37 -0400

    Improved add to checkout functionality

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { menuArray } from "/data.js";
 
+function buttonTest() {
+	alert("clicked!");
+}
+
 function returnMenuArray(menuArr) {
 	return menuArr
 		.map((item) => {
@@ -13,7 +17,7 @@ function returnMenuArray(menuArr) {
 						<p class="item-desc">${ingredients}</p>
 						<h4 class="item-price">$${price}</h4>
 					</div>
-						<button class="add-btn" data-id=${id} onclick="displayPreCheckout(id)">+</button>
+						<button class="add-btn" data-id=${id}">+</button>
 				</div>
         `;
 		})
@@ -22,21 +26,51 @@ function returnMenuArray(menuArr) {
 
 document.getElementById("itemList").innerHTML = returnMenuArray(menuArray);
 
-function displayPreCheckout(id) {
+function displayPreCheckout() {
 	const addBTN = document.querySelectorAll(".add-btn");
-	const preCheckout = document.getElementById("pre-checkout");
-	const title = document.createElement("h2");
-	title.innerText = "Your Order";
-	preCheckout.appendChild(title);
 
-	// console.log(addBTN);
-
-	menuArr.map((item) => {
+	let totalSumArray = [];
+	menuArray.map((item) => {
 		const { name, id, price } = item;
-		addBTN.forEach((element) => {
-			element.addEventListener("click", function () {
-				console.log(item.name, item.id, item.price);
+		addBTN.forEach((button) => {
+			button.addEventListener("click", function () {
+				const dataInfo = button.getAttribute("data-id");
+				if (parseInt(dataInfo) === id) {
+					//Create the checkout title and assign inner text
+					const checkoutTitle = document.getElementById("checkout-title");
+					checkoutTitle.innerText = "Your Order";
+
+					//Create the total price list-item that generated once an item is added to cart
+					const totalPrice = document.getElementById("total-price");
+
+					const checkoutList = document.getElementById("checkout-list");
+					const checkoutItem = document.createElement("li");
+					checkoutItem.innerHTML = `
+							${name} <button class="remove-item">remove</button>
+							<span class="item-price">$${price}</span>
+						`;
+					checkoutList.appendChild(checkoutItem);
+
+					// Push each item's price to the total Array created earlier
+					totalSumArray.push(price);
+
+					const totalPriceSum = totalSumArray.reduce((total, current) => {
+						return total + current;
+					}, 0);
+					totalPrice.innerHTML = `
+					Total Price: <span class="item-price">$${totalPriceSum}</span>
+					`;
+
+					const purchaseBTN = document.getElementById("purchase-btn");
+					purchaseBTN.style.display = "block";
+				}
 			});
 		});
 	});
 }
+
+function removeItem() {
+	
+}
+
+displayPreCheckout();
